159753cd9eb8e666d5f97e53a70474c4804a8c41
Zakar <zakar@local>
2026-07-24 23:46:00 +0200

    chore: bump versionCode to 66

--- app.config.ts
+++ app.config.ts
@@ -62,7 +62,7 @@ const config: ExpoConfig = {
     },
     edgeToEdgeEnabled: true,
     predictiveBackGestureEnabled: false,
-    versionCode: 65,
+    versionCode: 66,
     package: process.env.BUNDLE_IDENTIFIER || 'com.zkrstudio.invoicerapidpro',
     googleServicesFile: './google-services.json',
   },
